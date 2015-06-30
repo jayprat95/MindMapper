964e489b1465e8f3067d7313b8ea936dea3a9d21
Kyle <kyleh8@vt.edu>
2015-06-30 16:04:08 -0400

    fix the click listener

--- mindMapperApp/src/main/java/edu/engagement/application/MainActivity.java
+++ mindMapperApp/src/main/java/edu/engagement/application/MainActivity.java
@@ -27,8 +27,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.FrameLayout;
 
-import javax.xml.datatype.Duration;
-
 import edu.engagement.application.Database.DataPointSource;
 import edu.engagement.application.Fragments.BaselineFragment;
 import edu.engagement.application.Fragments.DatabaseFragment;
@@ -44,7 +42,7 @@ import edu.engagement.application.SlidingTab.*;
 /**
  * MainActivity June 16
  */
-public class MainActivity extends FragmentActivity implements View.OnClickListener{
+public class MainActivity extends FragmentActivity {
 
 
     public static final String BASELINE_AVG_KEY = "avg";
@@ -594,16 +592,6 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
     public boolean fabClicked() {
         return this.fabClicked;
-    }
-
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(this, v.getClass().getName().toString(), Toast.LENGTH_SHORT);
     }
 
     public enum state {
