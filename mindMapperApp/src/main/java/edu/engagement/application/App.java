de71a1022d1e2a24849e1bf82f29731476917764
Alex Marshall <amhokies@gmail.com>
2016-03-23 18:07:57 -0400

    Trashing Flurry, tested and confirmed that ACRA works, so we'll be moving on with that.

--- mindMapperApp/src/main/java/edu/engagement/application/App.java
+++ mindMapperApp/src/main/java/edu/engagement/application/App.java
@@ -1,26 +1,28 @@
 package edu.engagement.application;
 
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
 
-import com.flurry.android.FlurryAgent;
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
 
 /**
  * Created by alex on 7/27/15.
  */
+@ReportsCrashes(
+        formUri = "https://collector.tracepot.com/3e0ff4c0"
+)
 public class App extends Application {
     public static final String NAME = "MindMapper";
 
     @Override
-    public void onCreate() {
-        super.onCreate();
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
 
-        Log.d(NAME, "INITIALIZING FLURRY (" + FlurryAgent.getReleaseVersion() + ")");
+        Log.d(NAME, "Starting ACRA");
 
-        // configure Flurry
-        FlurryAgent.setLogEnabled(true);
-
-        // init Flurry
-        FlurryAgent.init(this, "B8PDQPHRM3TW74QS2ZHS");
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
     }
 }
