36004b338cc6439c57ccfcee6c9a05a542aa8868
Alex Marshall <amhokies@gmail.com>
2016-03-23 13:07:01 -0400

    Trying out Flurry

--- mindMapperApp/src/main/java/edu/engagement/application/App.java
+++ mindMapperApp/src/main/java/edu/engagement/application/App.java
@@ -1,8 +1,26 @@
 package edu.engagement.application;
 
+import android.app.Application;
+import android.util.Log;
+
+import com.flurry.android.FlurryAgent;
+
 /**
  * Created by alex on 7/27/15.
  */
-public class App {
+public class App extends Application {
     public static final String NAME = "MindMapper";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.d(NAME, "INITIALIZING FLURRY (" + FlurryAgent.getReleaseVersion() + ")");
+
+        // configure Flurry
+        FlurryAgent.setLogEnabled(true);
+
+        // init Flurry
+        FlurryAgent.init(this, "B8PDQPHRM3TW74QS2ZHS");
+    }
 }
